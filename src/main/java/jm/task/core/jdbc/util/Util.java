a64995c87e53f2f2ddaca42aa0efad2fc741813f
AntonWlM <146090601+AntonWlM@users.noreply.github.com>
2023-12-22 19:10:38 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -15,9 +15,9 @@ import java.util.Properties;
 
 public class Util {
 
-    private static final String dbUrl = "jdbc:mysql://localhost:3306/userbase;";   //"usersbase";
+    private static final String dbUrl = "jdbc:mysql://localhost:3306/usersbase";
     private static final String dbUserName = "root";
-    private static final String dbPass = "root"; //"my179sql";
+    private static final String dbPass = "123456789"; //"my179sql";
     Driver driver = new com.mysql.cj.jdbc.Driver();
     private static SessionFactory sessionFactory;
 
@@ -27,21 +27,7 @@ public class Util {
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
-                Configuration configuration = new Configuration();
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/userbase?useSSL=false");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, dbPass);
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-
-                settings.put(Environment.SHOW_SQL, "true");
-
-                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
-                settings.put(Environment.HBM2DDL_AUTO, "none");
-
-                configuration.setProperties(settings);
+                Configuration configuration = getConfiguration();
 
                 configuration.addAnnotatedClass(User.class);
 
@@ -54,6 +40,25 @@ public class Util {
             }
         }
         return sessionFactory;
+    }
+
+    private static Configuration getConfiguration() {
+        Configuration configuration = new Configuration();
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, "jdbc:mysql://localhost:3306/usersbase?useSSL=false");
+        settings.put(Environment.USER, "root");
+        settings.put(Environment.PASS, dbPass);
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+        settings.put(Environment.SHOW_SQL, "true");
+
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+        settings.put(Environment.HBM2DDL_AUTO, "none");
+
+        configuration.setProperties(settings);
+        return configuration;
     }
 
 
